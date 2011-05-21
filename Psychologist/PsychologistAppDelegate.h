fce0990fda375cdbc697ac2eac162b2b64885224
Mike Goodspeed <mgoodspeed@getcollc.com>
2011-05-21 09:20:48 +0100

    Initial Commit

---
+++ Psychologist/PsychologistAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PsychologistAppDelegate.h
+//  Psychologist
+//
+//  Created by Mike Goodspeed on 5/21/11.
+//  Copyright 2011 Mike Goodspeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
